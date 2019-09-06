4dac06198d85bd7c0f2d21e6153e8eca7e847093
Шадрин Александр Андреевич <shadrinaa@alt.altarix.ru>
2019-09-06 14:48:32 +0300

    Fixed loading json data.

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ const total = document.getElementById('total');
 
 let filter = '';
 let count = 0;
+let questions = [];
 
-const refreshRecords = (filter) => {
+document.addEventListener('keydown', (event) => {
+	if (event.code === 'Escape') {
+		input.focus();
+		input.value = "";
+		window.scrollTo({ top: 0, behavior: "smooth"});
+		filter = "";
+		refreshRecords("");
+	}
+});
+
+function refreshRecords(filter) {
 	container.innerHTML = "";
-
+	
 	const filtered_questions = questions.filter(({ question }) => (new RegExp(filter, 'i')).test(question));
 	count = filtered_questions.length;
 	total.innerHTML = `${filtered_questions.length} / ${questions.length}`;
-
-	filtered_questions.forEach(({ id, question, answer }, i) => {
+	
+	filtered_questions.forEach(({ id, question, answer }) => {
 		const div = document.createElement('div');
 		div.className = "question";
 		
@@ -23,28 +34,37 @@ const refreshRecords = (filter) => {
 		const ans = document.createElement('p');
 		ans.innerHTML = answer;
 		ans.className = "question-answer";
-
+		
 		div.appendChild(qwe);
 		div.appendChild(ans);
-
+		
 		container.appendChild(div);
 	});
-};
+}
 
-const handleChange = (target) => {
+function handleChange(target) {
 	filter = target.value;
 	refreshRecords(filter);
-};
+}
 
-document.addEventListener('keydown', (event) => {
-	if (event.code == 'Escape') {
-		input.focus();
-		input.value = "";
-		window.scrollTo({ top: 0, behavior: "smooth"});
-		filter = "";
+function loadJSON(path, callback) {
+	const obj = new XMLHttpRequest();
+	obj.overrideMimeType("application/json");
+	obj.open('GET', path, true);
+	obj.onreadystatechange = function () {
+		console.log('RES: ', obj);
+		if (obj.readyState === 4 && obj.status === 200) {
+			callback(JSON.parse(obj.responseText));
+		}
+	};
+	obj.send(null);
+}
+
+function init() {
+	loadJSON('./questions_pretty.json', function(response) {
+		questions = response;
 		refreshRecords("");
-	}
-});
+	});
+}
 
-refreshRecords("");
-
+init();
